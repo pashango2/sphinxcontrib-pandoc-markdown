1cfd6d609f15813823e39d0f76476524eba1812b
pashango <toshi19770331@gmail.com>
2017-05-11 23:17:51 +0900

    change

--- sphinxcontrib/pandoc_markdown.py
+++ sphinxcontrib/pandoc_markdown.py
@@ -1,56 +1,13 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
 import os
 import re
 from tempfile import mkstemp
 from six import PY2
-
-
-class MarkdownProcessor(object):
-    def on_source_read(self, app, docname, source):
-        if docname == 'index':
-            return
-
-        try:
-            input = mkstemp()
-            output = mkstemp()
-            os.close(input[0])
-            os.close(output[0])
-
-            with open(input[1], 'wt') as f:
-                if PY2:
-                    f.write(source[0].encode('utf-8'))
-                else:
-                    f.write(source[0])
-
-            to_opt = [
-                "-f", "markdown+raw_html+markdown_in_html_blocks+autolink_bare_uris"
-                      "+tex_math_single_backslash-implicit_figures",
-                "-t", "rst+raw_html-implicit_figures"
-            ]
-
-            cmdline = "pandoc %s -R -r markdown -w rst %s -o %s" % (" ".join(to_opt), input[1], output[1])
-            os.system(cmdline)
-
-            if PY2:
-                source[0] = open(output[1]).read().decode('utf-8')
-            else:
-                source[0] = open(output[1]).read()
-
-            source[0] = post_process(source[0])
-
-        finally:
-            os.unlink(input[1])
-            os.unlink(output[1])
-
-    def setup(self, app):
-        app.add_config_value('markdown_title', None, 'html')
-
-        app.connect('source-read', self.on_source_read)
-
-
-def setup(app):
-    md = MarkdownProcessor()
-    md.setup(app)
+from docutils import nodes
+from docutils import parsers
+from docutils.parsers.rst import Parser
 
 
 REPLACE_CODE_TYPES = {
@@ -106,3 +63,53 @@ def post_process(docs):
         new_docs.append(doc)
 
     return "\n".join(new_docs)
+
+
+class MarkdownParser(Parser):
+    PANDOC_OPT = [
+        "-f", "markdown+raw_html+markdown_in_html_blocks+autolink_bare_uris"
+              "+tex_math_single_backslash-implicit_figures",
+        "-t", "rst+raw_html-implicit_figures"
+    ]
+
+    def parse(self, inputstring, document):
+        outputstring = None
+
+        pre_code = []
+        input_string = []
+        for line in inputstring.splitlines():
+            if line.startswith(".. |"):
+                pre_code.append(line)
+            else:
+                input_string.append(line)
+
+        input_string = "\n".join(input_string)
+
+        try:
+            input = mkstemp()
+            output = mkstemp()
+            os.close(input[0])
+            os.close(output[0])
+
+            with open(input[1], 'wt') as f:
+                if PY2:
+                    f.write(input_string.encode('utf-8'))
+                else:
+                    f.write(input_string)
+
+            cmdline = "pandoc %s -R -r markdown -w rst %s -o %s" % (" ".join(self.PANDOC_OPT), input[1], output[1])
+            os.system(cmdline)
+
+            if PY2:
+                outputstring = open(output[1]).read().decode('utf-8')
+            else:
+                outputstring = open(output[1]).read()
+
+            outputstring = post_process(outputstring)
+
+        finally:
+            os.unlink(input[1])
+            os.unlink(output[1])
+
+        if outputstring:
+            super(MarkdownParser, self).parse(outputstring, document)
